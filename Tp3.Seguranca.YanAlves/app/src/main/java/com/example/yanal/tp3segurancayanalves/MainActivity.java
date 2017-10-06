e568c06059b5a98d286ecd25260788c72b7650f7
Yan Alves Monteiro <yanal@DESKTOP-A6FG6AP.(none)>
2017-10-06 01:41:40 -0300

    :construction: Finaliza tarefa proposta

---
+++ Tp3.Seguranca.YanAlves/app/src/main/java/com/example/yanal/tp3segurancayanalves/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.yanal.tp3segurancayanalves;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.InterstitialAd;
+
+public class MainActivity extends AppCompatActivity {
+    private InterstitialAd mInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mInterstitialAd = new InterstitialAd(this);
+        mInterstitialAd.setAdUnitId("ca-app-pub-3940256099942544/1033173712");
+        mInterstitialAd.loadAd(new AdRequest.Builder().build());
+
+        mInterstitialAd.setAdListener(new AdListener() {
+            @Override
+            public void onAdLoaded() {
+                mInterstitialAd.show();
+            }
+        });
+    }
+}
